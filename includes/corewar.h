fdba7e8519c2820d952c41c3b6ee8d3b92ada7bc
nalexand <alxdthn@gmail.com>
2019-08-05 14:54:00 +0300

    merge header

--- includes/corewar.h
+++ includes/corewar.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   corewar.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skrystin <skrystin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nalexand <nalexand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/03 21:12:57 by nalexand          #+#    #+#             */
-/*   Updated: 2019/08/05 14:39:23 by skrystin         ###   ########.fr       */
+/*   Updated: 2019/08/05 14:53:25 by nalexand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 # include "libft.h"
 # include "ft_printf.h"
 # include "op.h"
+
+# define MEM_ERROR "Error: mem alloc error!"
+# define OPEN_ERR "Error: can't open file!"
+# define INPUT_ERR "Error: input error!"
+
+# define STR(var) ((char *)var->content)
+
 /*
 **	common part
 */
@@ -33,20 +40,11 @@ typedef struct	s_op
 
 t_op    		op_tab[17];
 
-void			print_memory(char *av);
+void			print_memory(char *mem, ssize_t size);
 /*
 **	asm part
 */
-typedef	struct	s_as
-{
-	char		name[129];
-	int			name_i;
-	char		comment[2049];
-	int			com_i;
-	char		read;
-	int			size;
-	t_list		*comm;
-}				t_as;
+		
 /*
 **	corewar part
 */
@@ -54,6 +52,11 @@ typedef struct	s_core
 {
 	char		map[MEM_SIZE];
 	t_list		*input;
+	void		(*exit)(struct s_core *, const char *, const int);
 }				t_core;
+
+void			cw_clear_exit(t_core *core, const char *message, const int fd);
+void			print_map(char *map);
+void			print_input(t_list *tmp);
 
 #endif
