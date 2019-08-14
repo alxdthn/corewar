619ea9817c7c00e8ca6f4b7a345fa910bcb9cbef
IvanLevin <levvanya@gmail.com>
2019-08-14 22:32:06 +0300

    pars

--- src/corewar/visual/render.c
+++ src/corewar/visual/render.c
@@ -15,15 +15,15 @@
 int				is_carry_here(t_core *core, int i)
 {
 	t_list	    *pc;
-//    int			j;
+   int			j;
 
-//    j = 0;
+   j = 0;
 	pc = core->pcs;
 	while (pc)
 	{
 		if (PC->position == i)
 			return (i);
-//        j++;
+       j++;
 		pc = pc->next;
 	}
 	return (-1);
@@ -36,29 +36,17 @@ static void		print_arena_content(t_core *core, int i, int y, int x)
 	int id;
 
 	id = is_carry_here(core, i);
-//    if (id == -2)
-//    {
-//        attron(COLOR_PAIR(10));
-//        mvprintw(y, x, "%02x", core->map[i]);
-//        attroff(COLOR_PAIR(10));
-//    }
 	if (id != -1)
 	{
-		attron(COLOR_PAIR(GRAY_CURSOR));
+		attron(COLOR_PAIR(id % MAX_COLOR_NUM));
 		mvprintw(y, x, "%02x", core->map[i]);
-		attroff(COLOR_PAIR(GRAY_CURSOR));
-		//attron(COLOR_PAIR(core->visual.map[i].index));
-		//mvprintw(y, x, "%02x", core->map[i]);
-		//attroff(COLOR_PAIR(core->visual.map[i].index));
+		attroff(COLOR_PAIR(id % MAX_COLOR_NUM));
 	}
 	else
 	{
-		attron(COLOR_PAIR(GRAY));
+		attron(COLOR_PAIR(9));
 		mvprintw(y, x, "%02x", core->map[i]);
-		attroff(COLOR_PAIR(GRAY));
-		//attron(COLOR_PAIR(core->visual.map[i].index));
-		//mvprintw(y, x, "%02x", core->map[i]);
-		//attroff(COLOR_PAIR(core->visual.map[i].index));
+		attroff(COLOR_PAIR(9));
 	}
 }
 
@@ -71,7 +59,6 @@ void			render_arena(t_core *core)
 	i = 0;
 	x = 1;
 	y = 1;
-//    init_attr(core);
 	while (i < MEM_SIZE)
 	{
 		print_arena_content(core, i, y, x);
@@ -98,7 +85,6 @@ void	render_info(t_core *core)
 	c = 0;
 	while (core->players[i])
 	{
-//        printf("ss %d\n", core->war_count);
 		if (core->players[i]->nb == (core->war_count - \
 		(core->war_count - core->players[i]->nb)))
 		{
@@ -106,7 +92,6 @@ void	render_info(t_core *core)
 			mvprintw(3 + z, 198, "Player %d:", core->players[i]->nb);
 			attroff(COLOR_PAIR(1 + c));
 			mvprintw(4 + z, 198, core->players[i]->name);
-//            mvprintw(5 + z, 198, "Last live: %d", core->last_player);
 		}
 		i++;
 		z = z + 10;
@@ -115,7 +100,7 @@ void	render_info(t_core *core)
 }
 
 void		render_window(t_core *core)
-{
+{ 
 	render_info(core);
 	render_arena(core);
 	mvprintw(50, 198, "Cycle : %d", core->cycle_after_start);
