73d3e4b083fd170151270651521eb20666886b96
Sturgis Krystina <skrystin@mi-q2.21-school.ru>
2019-08-16 17:50:07 +0300

    fix

--- src/asm/command_val.c
+++ src/asm/command_val.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command_val.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nalexand <nalexand@student.42.fr>          +#+  +:+       +#+        */
+/*   By: skrystin <skrystin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 17:10:00 by skrystin          #+#    #+#             */
-/*   Updated: 2019/08/16 05:27:03 by nalexand         ###   ########.fr       */
+/*   Updated: 2019/08/16 17:49:39 by skrystin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ int				add_label(t_as **all, char **f, char *str, int y)
 	t_label	*lab;
 
 	i = 0;
-	while (*str == ' ' || *str == '\t')
-		str++;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
 	while (ft_strindex(LABEL_CHARS, str[i]) != -1)
 		i++;
 	if (str[i] != LABEL_CHAR)
